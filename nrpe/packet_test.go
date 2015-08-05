9297e5a64b5b696485a405766f323869dcebe7b5
Otavio Fernandes <ofernandes@schubergphilis.com>
2015-08-05 04:18:13 +0200

    Making it silent for now.

--- nrpe/packet_test.go
+++ nrpe/packet_test.go
@@ -1,12 +1,13 @@
 package nrpe_test
 
 import (
-	. "github.com/smartystreets/goconvey/convey"
-    "github.com/otaviof/godutch/nrpe"
+	// . "github.com/smartystreets/goconvey/convey"
+    // "github.com/otaviof/godutch/nrpe"
 	"testing"
 )
 
 func TestPacketExtract(t *testing.T) {
+    // nrpe_packet := nrpe
 }
 
 /* EOF */
