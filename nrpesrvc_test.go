ade84a218cbb636e623b1e834b9eb802275c38c0
Otavio Fernandes <ofernandes@schubergphilis.com>
2015-12-28 16:01:27 +0100

    Reusing NRPE packet bytes and testing NRPE port.

--- nrpesrvc_test.go
+++ nrpesrvc_test.go
@@ -1,8 +1,10 @@
 package godutch_test
 
 import (
+	"fmt"
 	. "github.com/otaviof/godutch"
 	. "github.com/smartystreets/goconvey/convey"
+	"net"
 	"testing"
 )
 
@@ -11,9 +13,14 @@ func TestNewNRPESrvc(t *testing.T) {
 	var cfg *Config
 	var g *GoDutch
 	var ns *NRPESrvc
+	var conn net.Conn
+	var listenOn string
+	var n int
 
 	g = NewGoDutch()
 	cfg, _ = NewConfig("__etc/godutch/godutch.ini")
+	// same string used on service object
+	listenOn = fmt.Sprintf("%s:%d", cfg.NRPE.Interface, cfg.NRPE.Port)
 	ns, err = NewNRPESrvc(&cfg.NRPE, g)
 
 	g.Register(ns)
@@ -23,6 +30,18 @@ func TestNewNRPESrvc(t *testing.T) {
 		err = g.Onboard(ns)
 		So(err, ShouldEqual, nil)
 	})
+
+	Convey("Should be have the port open, and accepting payload", t, func() {
+		conn, err = net.Dial("tcp", listenOn)
+		So(err, ShouldEqual, nil)
+		n, err = conn.Write(CHECK_NRPE_PAYLOAD)
+		So(n, ShouldEqual, NRPE_PACKET_SIZE)
+		So(err, ShouldEqual, nil)
+		err = conn.Close()
+		So(err, ShouldEqual, nil)
+	})
+
+	g.Stop()
 }
 
 /* EOF */
