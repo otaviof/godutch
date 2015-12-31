9f9873d4e2a1b92430fc32904408bdfde35facbd
Otavio Fernandes <ofernandes@schubergphilis.com>
2015-12-31 19:03:58 +0100

    Declaring types.

--- protocol_test.go
+++ protocol_test.go
@@ -7,19 +7,26 @@ import (
 )
 
 func TestNewRequest(t *testing.T) {
+	var err error
+	var req []byte
+	var reqStr string
+
 	Convey("Should be able to instantiate a new Request", t, func() {
-		payload, err := NewRequest("test", []string{})
+		req, err = NewRequest("test", []string{})
 		So(err, ShouldEqual, nil)
-		payloadStr := string(payload[:])
-		So(payloadStr, ShouldEqual, "{\"command\":\"test\",\"arguments\":[]}\n")
+		reqStr = string(req[:])
+		So(reqStr, ShouldEqual, "{\"command\":\"test\",\"arguments\":[]}\n")
 	})
 }
 
 func TestNewResponseListCheckMethods(t *testing.T) {
+	var err error
+	var req []byte = []byte(
+		"{\"name\":\"__list_check_methods\",\"stdout\":[\"check_test\",\"check_second_test\"]}")
+	var resp *Response
+
 	Convey("Should be able to new Response '__list_check_methods'", t, func() {
-		payload := []byte(
-			"{\"name\":\"__list_check_methods\",\"stdout\":[\"check_test\",\"check_second_test\"]}")
-		resp, err := NewResponse(payload)
+		resp, err = NewResponse(req)
 		So(err, ShouldEqual, nil)
 		So(resp.Name, ShouldEqual, "__list_check_methods")
 	})
