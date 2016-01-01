9a3bbc8f0bf59e687395dc83ce3dcad84630975d
Otavio Fernandes <ofernandes@schubergphilis.com>
2016-01-01 19:22:03 +0100

    Comments and such.

--- service.go
+++ service.go
@@ -1,9 +1,18 @@
+/*
+
+Abstract:
+  A GoDutch Service is a instance that whatches the actual running service,
+  informed by parameters, through Composer interface. Here it's expected to
+  execute the boilerplates in order to run the actual service, and map what's
+  informed by configuration into the Service based Go Types.
+
+*/
+
 package godutch
 
 //
 // Type Service must implement Composer interface and keep track of the running
-// service instance (Suture interface compatible) running underneath. Here the
-// actual service boostrap should take place.
+// service instance (Suture interface compatible) running underneath.
 //
 type Service struct {
 	Name string
@@ -20,6 +29,19 @@ func NewService(cfg *ServiceConfig, g *GoDutch) *Service {
 	//    hardcoded NrpeService being managed;
 	s.Srvc = NewNrpeService(cfg, g)
 	return s
+}
+
+// Displays the name for this service (component), Composer interface.
+func (s *Service) ComponentInfo() *Component {
+	var component *Component
+	component = &Component{
+		Name:   s.Name,
+		Checks: []string{},
+		Type:   "service",
+		// running service instance
+		Instance: s.Srvc,
+	}
+	return component
 }
 
 // Dummy method on Service, there's no bootstrap here.
@@ -39,18 +61,6 @@ func (n *Service) Execute(req []byte) (*Response, error) {
 	var err error
 	var resp *Response = &Response{}
 	return resp, err
-}
-
-// Displays the name for this service (component).
-func (s *Service) ComponentInfo() *Component {
-	var component *Component
-	component = &Component{
-		Name:     s.Name,
-		Checks:   []string{},
-		Type:     "service",
-		Instance: s.Srvc,
-	}
-	return component
 }
 
 /* EOF */
