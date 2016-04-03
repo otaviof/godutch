3d736d2be84ad3c0df91fab5de59b6e50a0e0101
Otavio Fernandes <ofernandes@schubergphilis.com>
2016-04-03 03:06:31 +0200

    New module to manage the Containers, it's Panamax!

---
+++ panamax.go
@@ -0,0 +1,103 @@
+package godutch
+
+//
+// Panamax is a world class container carrier vessel, and here the analogy
+// applies for holding the check containers using Supervisor.
+// On GoDutch this has one more role, to route the check requests towards the
+// right container, since it keeps the inventory.
+//
+
+import (
+	"github.com/thejerf/suture"
+	"log"
+	"errors"
+	"time"
+)
+
+//
+// Containers and Checks inventory, plus Supervisor structure.
+//
+type Panamax struct {
+	*suture.Supervisor
+	// maps a container name to it's instance
+	containers map[string]*Container
+	// maps a check name to it's container instance
+	checks map[string]*Container
+}
+
+// Creates a new Panamax instnace. Alocates memotry and loads a new supervisor
+// instance to hold the Containers.
+func NewPanamax() (*Panamax, error) {
+	var p *Panamax = &Panamax{
+		Supervisor: suture.New("Panamax", suture.Spec{
+			Log: func(line string) { log.Println("[SUTURE-Panamax]", line) },
+		}),
+		containers: make(map[string]*Container),
+		checks:make(map[string]*Container),
+	}
+
+	// letting the Supervisor run in background right from the start, it will be
+	// requested to be on running state when onboarding containers
+	go p.ServeBackground()
+	
+	return p, nil
+}
+
+// Loads a container based on configuration, starting command in background and
+// loading it's inventory right after. When Container has no checks it will
+// return error.
+func (p *Panamax) Load(cfg *ContainerConfig) (error) {
+	var found bool = false
+	var item string
+	var err error
+
+	log.Printf("[Panamax] Loading container: '%s'", cfg.Name)
+	if _, found = p.containers[cfg.Name]; found {
+		return errors.New("[Panamax] Container already loaded: " + cfg.Name)
+	}
+
+	if p.containers[cfg.Name], err = NewContainer(cfg); err != nil {
+		return err
+	}
+
+	// loading container on local Supervisor and quick sleep, to give it time to
+	// start and be able to respond
+	p.Add(p.containers[cfg.Name].Client())
+	time.Sleep(1e9)
+
+	if err = p.containers[cfg.Name].Bootstrap(); err != nil {
+		log.Printf("[Panamax] Error on boostrapping container")
+		return err
+	}
+
+	// having no checks found on this continer will return error
+	if len(p.containers[cfg.Name].Inventory()) <= 0 {
+		err = errors.New("[Panamax] No inventory found on: " + cfg.Name)
+		return err
+	}
+
+	// loading container inventory
+	for _, item = range p.containers[cfg.Name].Inventory() {
+		log.Printf("[Panamax] Container '%s' has check: '%s'", cfg.Name, item)
+		p.checks[item] = p.containers[cfg.Name]
+	}
+
+	return nil
+}
+
+// Wraps the Execute method from the Container using local inventory.
+func (p *Panamax) Execute(req *Request) (*Response, error) {
+	var name string = req.Fields.Command
+	var found bool = false
+	var err error
+
+	// check's command is it's name, can be found on Request's fields
+	if _, found = p.checks[name]; !found {
+		err = errors.New("[Panamax] Can't find a check named:" + name)
+		return nil, err
+	}
+
+	return p.checks[name].Execute(req)
+}
+
+/* EOF */
---
+++ panamax_test.go
@@ -0,0 +1,46 @@
+package godutch_test
+
+import (
+	. "github.com/otaviof/godutch"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func mockPanamax(t *testing.T) (*Panamax) {
+	var p *Panamax
+	var err error
+
+	Convey("Should be able to instantiate Panamax", t, func () {
+		p, err = NewPanamax()
+		So(err, ShouldEqual, nil)
+	})
+
+	return p
+}
+
+func TestLoadAndExecute(t *testing.T) {
+	var p *Panamax = mockPanamax(t)
+	var cfg *Config = mockNewConfig(t)
+	var req *Request
+	var resp *Response
+	var name string
+	var err error
+
+	Convey("Should load a container", t, func() {
+		err = p.Load(cfg.Containers["rubycontainer"])
+		So(err, ShouldEqual, nil)
+	})
+
+	Convey("Should Execute a Checks using the Panamax's routing", t, func() {
+		for _, name = range []string{"check_test", "check_second_test"} {
+		  req, err = NewRequest(name, []string{})
+		  So(err, ShouldEqual, nil)
+
+		  resp, err = p.Execute(req)
+		  So(err, ShouldEqual, nil)
+		  So(resp.Name, ShouldEqual, req.Fields.Command)
+		}
+	})
+}
+
+/* EOF */
