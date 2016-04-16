9e2873a0b7b242373c3066ad41b320456b6c4e72
Otavio Fernandes <ofernandes@schubergphilis.com>
2016-04-16 07:34:13 +0200

    Placeholders.

---
+++ cmd/godutch-cli/main.go
@@ -0,0 +1,10 @@
+package godutch_cli
+
+//
+// godutch-cli is the command line interface for GoDutch daemon, with the
+// following roles:
+//   - control the godutch daemon, start, stop, restart;
+//   - call for ad-hoc check execution;
+//   - load/unload containers (include and remove checks);
+//   - display the daemon statistics;
+//
